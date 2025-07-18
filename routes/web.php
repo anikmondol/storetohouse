0af692bc637799eb36aeb057999b80aa29c1d208
anikmondol <anikmondol558363@gmail.com>
2025-07-18 22:16:35 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\FrontendController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ManagementController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Auth::routes(['register' => false]);
+
+Route::get('/',[FrontendController::class,'index'])->name('root');
+
+
+// dashboard routes
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+
+// management
+
+Route::prefix(env('HOST_NAME'))->middleware(['rolecheck'])->group(function(){
+    Route::get('/management', [ManagementController::class, 'index'])->name('management.index');
+    Route::post('/management/user/register', [ManagementController::class, 'store_register'])->name('management.store');
+    Route::post('/management/user/manager/down/{id}', [ManagementController::class, 'manager_down'])->name('management.down');
+
+    //role
+    Route::get('/management/role', [ManagementController::class, 'role_index'])->name('management.role.index');
+    Route::post('/management/role/assign', [ManagementController::class, 'role_assign'])->name('management.role.assign');
+    Route::post('/management/role/undo/blogger/{id}', [ManagementController::class, 'blogger_grade_down'])->name('management.role.blogger.down');
+    Route::post('/management/role/undo/user/{id}', [ManagementController::class, 'user_grade_down'])->name('management.role.user.down');
+
+});
+
+
+
+// profile
+
+Route::get("/home/profile",[ProfileController::class,'index'])->name('home.profile');
+Route::post("/home/profile/name/update",[ProfileController::class,'name_update'])->name('home.profile.name.update');
+Route::post("/home/profile/password/update",[ProfileController::class,'password_update'])->name('home.profile.password.update');
+Route::post("/home/profile/image/update",[ProfileController::class,'image_update'])->name('home.profile.image.update');
+
+
+
+
+
